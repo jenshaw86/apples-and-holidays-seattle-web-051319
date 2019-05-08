cf409de7d5dd37745ba13e34f091df5648368527
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-08 05:11:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts season_string
     
     holidays.each do |holiday, supplies|
-      holiday_string = "#{holiday.to_s.split("_").join.capitalize}: "
+      holiday_string = "#{holiday.to_s.split("_").capitalize.join}: "
       print holiday_string.chomp
       binding.pry
       supplies.each_with_index do |item, index|
