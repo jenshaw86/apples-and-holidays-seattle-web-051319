8f3b17a94912d9f05ff24ef69650ed13f04a0455
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-08 04:30:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ def all_winter_holiday_supplies(holiday_hash)
   
   holiday_hash[:winter].each do |holiday|
     winter_supplies << holiday.values  
-
+  end
 end
 
 def all_supplies_in_holidays(holiday_hash)
