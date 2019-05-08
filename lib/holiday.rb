c25e83e548b1db8ae2ed51d459036cbb8092e4cd
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-08 04:39:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,9 +46,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  winter_supplies = []
   
-  winter_supplies << holiday_hash[:winter].values.flatten
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
