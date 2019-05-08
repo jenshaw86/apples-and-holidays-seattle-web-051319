2004d46a7011ba6b80481ea340a4f32562e52d0f
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-08 05:23:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
     
     holidays.each do |holiday, supplies|
       holiday_string = "#{holiday.to_s.split("_").map(&:capitalize).join}: "
-      print " holiday_string"
+      print " #{holiday_string}: "
       supplies.each_with_index do |item, index|
         item_string = "#{item.to_s}"
         if index != supplies.length - 1
