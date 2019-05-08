8421b2ef60f984eb37c8fff2462034021b7c5f0d
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-08 04:41:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts season.to_s
+    puts season.to_s.
   end
 end
 
